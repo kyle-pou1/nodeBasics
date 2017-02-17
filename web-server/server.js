dd2218360c8e223ca0a356dec60f837abe6f92a8
Macbook <kyle.poul@gmail.com>
2017-02-17 16:42:45 -0700

    update for heroku

--- web-server/server.js
+++ web-server/server.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var app = express();
 
-
+var middleware = require('./middleware')
 //route level middle ware.
-var middleware = {
-  requireAuthentication: function(req, res, next) {
-    console.log('private route hit');
-    next();
-  },
-  logger: function (req, res, next) {
-    console.log('Request:', new Date().toString(), req.method, req.originalUrl);
-    next();
-  }
-}
+
 app.use(middleware.logger);//app.use gets used on entire app both root and about routes willprint out private route hit
 
 app.use(express.static('public'))
@@ -32,7 +23,7 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 
 
 
-var port = 3000;
+var port = process.env.port || 3000;
 app.listen(port, function(){
   console.log('I hear you on port:', port);
 })
